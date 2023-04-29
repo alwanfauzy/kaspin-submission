c5e41361f67c678083398f9edb4ab5e246426d00
alwanfauzy <alwanfauzi13@gmail.com>
2023-04-29 14:59:01 +0700

    create unit testing on core module

--- core/src/main/java/com/alwan/core/util/Dummy.kt
+++ core/src/main/java/com/alwan/core/util/Dummy.kt
@@ -1,5 +1,6 @@
 package com.alwan.core.util
 
+import com.alwan.core.data.local.entity.BarangEntity
 import com.alwan.core.domain.model.Barang
 
 object Dummy {
@@ -47,6 +48,58 @@ object Dummy {
         ),
         Barang(
             code = "aaabbbcc5",
+            name = "Barang5",
+            description = "description5",
+            stock = 1,
+            price = 1,
+            imageUrl = "www.gambar.com",
+        ),
+    )
+
+    fun generateBarangEntity() = BarangEntity(
+        id = "aaabbbcc1",
+        name = "Barang1",
+        description = "description1",
+        stock = 1,
+        price = 1,
+        imageUrl = "www.gambar.com",
+    )
+
+    fun generateListBarangEntities() = listOf(
+        BarangEntity(
+            id = "aaabbbcc1",
+            name = "Barang1",
+            description = "description1",
+            stock = 1,
+            price = 1,
+            imageUrl = "www.gambar.com",
+        ),
+        BarangEntity(
+            id = "aaabbbcc2",
+            name = "Barang2",
+            description = "description2",
+            stock = 1,
+            price = 1,
+            imageUrl = "www.gambar.com",
+        ),
+        BarangEntity(
+            id = "aaabbbcc3",
+            name = "Barang3",
+            description = "description3",
+            stock = 1,
+            price = 1,
+            imageUrl = "www.gambar.com",
+        ),
+        BarangEntity(
+            id = "aaabbbcc4",
+            name = "Barang4",
+            description = "description4",
+            stock = 1,
+            price = 1,
+            imageUrl = "www.gambar.com",
+        ),
+        BarangEntity(
+            id = "aaabbbcc5",
             name = "Barang5",
             description = "description5",
             stock = 1,
---
+++ core/src/test/java/com/alwan/core/data/repository/BarangRepositoryTest.kt
@@ -0,0 +1,86 @@
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.asLiveData
+import com.alwan.core.data.local.datasource.BarangDataSource
+import com.alwan.core.data.mapper.BarangMapper
+import com.alwan.core.data.repository.BarangRepositoryImpl
+import com.alwan.core.domain.repository.BarangRepository
+import com.alwan.core.util.Dummy
+import com.alwan.core.util.MainDispatcherRule
+import com.alwan.core.util.getOrAwaitValue
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class BarangRepositoryTest {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Mock
+    private lateinit var barangDataSource: BarangDataSource
+
+    private lateinit var barangMapper: BarangMapper
+    private lateinit var barangRepository: BarangRepository
+
+    @Before
+    fun setUp() {
+        barangMapper = BarangMapper()
+        barangRepository = BarangRepositoryImpl(barangDataSource, barangMapper)
+    }
+
+    @Test
+    fun `getAllBarang() should return list of barang`() = runTest {
+        val dummyListBarangEntities = Dummy.generateListBarangEntities()
+        val dummyListBarang = barangMapper.mapEntitiesToDomains(dummyListBarangEntities)
+
+        val expected = flowOf(dummyListBarangEntities)
+        `when`(barangDataSource.getAllBarang()).thenReturn(expected)
+
+        val actual = barangRepository.getAllBarang().asLiveData().getOrAwaitValue()
+
+        verify(barangDataSource).getAllBarang()
+        assertEquals(dummyListBarang.size, actual.size)
+        assertEquals(dummyListBarang[0], actual[0])
+        assertEquals(dummyListBarang[0].code, actual[0].code)
+        assertEquals(dummyListBarang[0].name, actual[0].name)
+        assertEquals(dummyListBarang[0].description, actual[0].description)
+        assertEquals(dummyListBarang[0].price, actual[0].price)
+        assertEquals(dummyListBarang[0].stock, actual[0].stock)
+        assertEquals(dummyListBarang[0].imageUrl, actual[0].imageUrl)
+    }
+
+    @Test
+    fun `getBarangById() should return Barang with id requested`() = runTest {
+        val barangId = "barangId"
+        val dummyBarangEntity = Dummy.generateBarangEntity()
+        val dummyBarang = barangMapper.mapEntityToDomain(dummyBarangEntity)
+
+        val expected = flowOf(dummyBarangEntity)
+        `when`(barangDataSource.getBarangById(barangId)).thenReturn(expected)
+
+        val actual = barangRepository.getBarangById(barangId).asLiveData().getOrAwaitValue()
+
+        verify(barangDataSource).getBarangById(barangId)
+        assertEquals(dummyBarang, actual)
+        assertEquals(dummyBarang.code, actual.code)
+        assertEquals(dummyBarang.name, actual.name)
+        assertEquals(dummyBarang.description, actual.description)
+        assertEquals(dummyBarang.price, actual.price)
+        assertEquals(dummyBarang.stock, actual.stock)
+        assertEquals(dummyBarang.imageUrl, actual.imageUrl)
+    }
+}
